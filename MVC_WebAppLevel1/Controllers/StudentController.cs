c1fcc39b1a07760411fc97a906e87c222c4dd7f4
abdoashraf221095 <abdoashraf221095@gmail.com>
2024-06-29 16:09:40 +0300

    Add and Prepare StudentController by Creating Index & Details Actions

---
+++ MVC_WebAppLevel1/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_WebAppLevel1.Models;
+
+namespace MVC_WebAppLevel1.Controllers
+{
+    public class StudentController : Controller
+    {
+        Students students = new();
+
+        public IActionResult Index()
+        {
+            return View("Index", students.GetAllStudents());
+        }
+
+        public IActionResult Details(int id)
+        {
+            Student? student = students.GetStudent(id);
+            return View("Deatils", student);
+        }
+    }
+}
