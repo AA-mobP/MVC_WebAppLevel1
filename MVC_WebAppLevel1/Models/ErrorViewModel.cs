b172836ddfd3b7ed31dc2b203e126a3746e34752
abdoashraf221095 <abdoashraf221095@gmail.com>
2024-06-29 15:15:01 +0300

    Add project files.

---
+++ MVC_WebAppLevel1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_WebAppLevel1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
